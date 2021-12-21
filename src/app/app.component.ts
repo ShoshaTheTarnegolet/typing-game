b43cb54b58d44a5c01c22979930217fc57bd15d3
Jenny <evgeniia.vader@gmail.com>
2021-12-21 15:09:19 +0200

    made a simple typing app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  /* https://rawgit.com/Marak/faker.js/master/examples/browser/index.html#lorem - link to fake demo, when you can choose a method */
+  solved = false;
+  inputText = '';
+
+  checkInput(value: string) {
+    this.inputText = value;
+    if (value === this.randomText) {
+      this.solved = true;
+    }
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+
+  /*
+this is first solution
+inputText = '';
+
+checkInput(value: string) {
+
+ this.inputText = value;
+if(this.inputText == this.randomText) {
+  console.log(this.inputText)
+}
+} */
 }
